dcb04ba7f008f8942957ceef8d3b191deca1b08c
Susumu Yamazaki <zacky1972@gmail.com>
2022-09-05 15:43:39 +0900

    set up elixir_make

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule NifSendSample.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      compilers: [:elixir_make] ++ Mix.compilers
     ]
   end
 
