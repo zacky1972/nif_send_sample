d46db8fcd14fdd2dc7ff7fde9a348c750dba56bb
Susumu Yamazaki <zacky1972@gmail.com>
2022-09-05 16:06:10 +0900

    implement send_nif

--- nif_src/libnif.c
+++ nif_src/libnif.c
@@ -1,7 +1,26 @@
+#include <stdbool.h>
 #include <erl_nif.h>
 
 static ERL_NIF_TERM send_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
 {
+    if(__builtin_expect(argc != 2, false)) {
+        return enif_make_badarg(env);
+    }
+
+    ErlNifPid pid;
+    if(__builtin_expect(!enif_get_local_pid(env, argv[0], &pid), false)) {
+        return enif_make_badarg(env);
+    }
+
+    ErlNifEnv *msg_env = enif_alloc_env();
+    if(__builtin_expect(msg_env == NULL, false)) {
+        return enif_make_badarg(env);
+    }
+    ERL_NIF_TERM message = argv[1];
+    if(__builtin_expect(!enif_send(NULL, &pid, msg_env, message), false)) {
+        return enif_make_tuple2(env, enif_make_atom(env, "error"), enif_make_string(env, "Fail to send.", ERL_NIF_LATIN1));
+    }
+
     return enif_make_atom(env, "ok");
 }
 
--- test/nif_send_sample_test.exs
+++ test/nif_send_sample_test.exs
@@ -1,4 +1,10 @@
 defmodule NifSendSampleTest do
   use ExUnit.Case
   doctest NifSendSample
+
+  test "send_nif(self(), :ping)" do
+    refute_receive(:ping, 100)
+    NifSendSample.send_nif(self(), :ping)
+    assert_receive(:ping, 1_000)
+  end
 end
