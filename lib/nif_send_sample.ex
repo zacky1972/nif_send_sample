8089a07fcab2440f30e1d3e70621b9bffe646ffb
Susumu Yamazaki <zacky1972@gmail.com>
2022-09-05 15:54:05 +0900

    temporal implementation of send_nif

--- lib/nif_send_sample.ex
+++ lib/nif_send_sample.ex
@@ -1,7 +1,24 @@
 defmodule NifSendSample do
+  require Logger
+
   @moduledoc """
   Documentation for `NifSendSample`.
   """
+
+  @on_load :load_nif
+
+  @doc false
+  def load_nif do
+    nif_file = ~c'#{Application.app_dir(:nif_send_sample, "priv/libnif")}'
+
+    case :erlang.load_nif(nif_file, 0) do
+      :ok -> :ok
+      {:error, {:reload, _}} -> :ok
+      {:error, reason} -> Logger.error("Failed to load NIF: #{inspect(reason)}")
+    end
+  end
+
+  def send_nif(_pid, _message), do: :erlang.nif_error(:not_loaded)
 
   @doc """
   Hello world.
--- nif_src/libnif.c
+++ nif_src/libnif.c
@@ -1,1 +1,13 @@
 #include <erl_nif.h>
+
+static ERL_NIF_TERM send_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
+{
+    return enif_make_atom(env, "ok");
+}
+
+static ErlNifFunc nif_funcs [] =
+{
+    {"send_nif", 2, send_nif},
+};
+
+ERL_NIF_INIT(Elixir.NifSendSample, nif_funcs, NULL, NULL, NULL, NULL)
