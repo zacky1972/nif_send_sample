2213b2f0c8a6835b5ddbb59019e90f098d87b6fa
Susumu Yamazaki <zacky1972@gmail.com>
2022-09-05 16:07:53 +0900

    add test spawn send_nif

--- test/nif_send_sample_test.exs
+++ test/nif_send_sample_test.exs
@@ -7,4 +7,11 @@ defmodule NifSendSampleTest do
     NifSendSample.send_nif(self(), :ping)
     assert_receive(:ping, 1_000)
   end
+
+  test "spawn send_nif" do
+    refute_receive(:ping, 100)
+    pid = self()
+    spawn(fn -> NifSendSample.send_nif(pid, :ping) end)
+    assert_receive(:ping, 1_000)
+  end
 end
